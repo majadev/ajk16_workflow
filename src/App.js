adaf876797fd47b5b346a092429b40c50a6c90b0
majadev <mgedda@live.se>
2017-11-29 12:15:39 +0100

    fetched pixabay api

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
+import ImgList from './Components/ImgList';
 
 
-class App extends Component {
+export default class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			imgs: []
+		};
+	}
 
-  componentWillMount() {
-    
-    fetch("https://dog.ceo/api/breeds/list/all")
-    .then(response => response.json())
-    .then(responseData => {
-        console.log(responseData)
-    })
-    .catch(error => {
-        console.log(' - Error fetching and parsing data', error);
-    });
-  }
+	componentDidMount() {
+		fetch('https://pixabay.com/api/?key=4521698-0a0bb4f2bcb4188eb3a7646d7&q=yellow+flowers&image_type=photo&pretty=true')
+			.then(res => res.json())
+			.then(data => {
+				this.setState({ imgs: data.hits });
+			})
+			.catch(err => {
+				console.log('Error happened during fetching!', err);
+			});
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Snel hezt</h1>
-        
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div className="main-header">
+					<div className="inner">
+						<h1 className="main-title">ImageSearch</h1>
+					</div>
+				</div>
+				<div className="main-content">
+					<ImgList data={this.state.imgs} />
+				</div>
+			</div>
+		);
+	}
 }
-
-export default App;
